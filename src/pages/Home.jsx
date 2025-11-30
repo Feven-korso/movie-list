7247361fa4f4270b220520bc045fafd94108bf95
fev <fevenkorso21@gmail.com>
2025-11-30 17:06:58 +0300

    update Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,22 @@
 import MovieCard from "../components/MovieCard"
+import { useState } from 'react';
 
 function Home(){
+    const [searchQuery, setSearchQuery] = useState("");
     const movie = [
         {id: 1, title: "sheldon", release_date: "2024"},
         {id: 2, title: "eldon", release_date: "2024"},
         {id: 3, title: "eldon", release_date: "2024"},
         {id: 4, title: "don", release_date: "2024"},]
-    const handleSearch = () =>{};
+    const handleSearch = () =>{
+        alert(searchQuery)
+    };
 
     return <div className="home">
         <form onSubmit={handleSearch} className="search-form">
-            <input type="text" placeholder="search for movie.." className="search-input" />
+            <input type="text" placeholder="search for movie.." className="search-input"  value={searchQuery} 
+            onChange={(e) => setSearchQuery(e.target.value)
+            }/>
             <button type="submit" className="search-button">search</button>
         </form>
         <div className="movie-grid">
